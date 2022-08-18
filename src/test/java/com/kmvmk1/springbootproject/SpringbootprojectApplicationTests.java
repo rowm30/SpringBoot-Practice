0a56db9a3909f3469013f771775495a817ec06d6
rowm30 <rowm30@gmail.com>
2022-08-18 18:16:18 +0530

    18Aug

---
+++ src/test/java/com/kmvmk1/springbootproject/SpringbootprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmvmk1.springbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
